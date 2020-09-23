74bc364bb4512fecf3c3f22c1db6d4c8b4281fe1
HARSHINI GADIGE <42682228+harshini5@users.noreply.github.com>
2020-09-23 02:08:14 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -15,12 +15,26 @@ class App extends Component {
     API.getAPIResponse().then((res) => {
     });
   }
+  handleClick = () => {
+    axios.get(`https://jsonmock.hackerrank.com/datetime`).then( resp => {
+     const getAPIDate = new Date(resp.data.date);
+     const getYear = getAPIDate.getFullYear();
+     const getMonth = getAPIDate.getMonth();
+     const getDay = getAPIDate.getDay();
+     this.setState({
+      getYear,
+      getMonth,
+      getDay
+     });
+    });
+  };
 
+  
   render() {
     return (
       <div>
         <center><h1>Date API</h1></center>
-        <center><DateButton></DateButton></center>
+        <center><DateButton handleClick={this.handleClick}></DateButton></center>
         <DateDisplay></DateDisplay>
       </div>
     );
